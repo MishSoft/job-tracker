1b36b1a9c94990d044ad1c7250beebcbb64fd4f7
Mishiko Aspanidze <120841642+MishSoft@users.noreply.github.com>
2026-08-31 21:28:17 +0400

    feat: add login endpoint with JWT and auth middleware

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { registerUser } from "../services/auth.service"
+import { registerUser, loginUser } from "../services/auth.service";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -7,7 +7,19 @@ export const register = async (req: Request, res: Response) => {
     const user = await registerUser(name, lastName, email, password)
     res.status(201).json(user)
   } catch (error) {
+    console.error(error)
     res.status(500).json({message: "Something went wrong!"})
   }
 
+}
+
+export const login = async (req: Request, res:Response) => {
+  try {
+    const { email, password } = req.body
+    const user = await loginUser(email, password)
+    res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: "Login error"})
+  }
 }
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,16 +1,48 @@
-import bcrypt from "bcrypt"
-import {prisma} from "../config/db"
-
-export const registerUser = async (name: string, lastName: string, email: string, password: string) => {
-  const hashedPassword = await bcrypt.hash(password, 10)
+import bcrypt from "bcrypt";
+import { prisma } from "../config/db";
+import jwt from "jsonwebtoken";
+export const registerUser = async (
+  name: string,
+  lastName: string,
+  email: string,
+  password: string,
+) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({
     data: {
       name,
       lastName,
       email,
-      password: hashedPassword
-    }
-  })
-  const { password: _, ...userWithoutPassword } = user
+      password: hashedPassword,
+    },
+  });
+  const { password: _, ...userWithoutPassword } = user;
   return userWithoutPassword;
-}
+};
+
+export const loginUser = async (email: string, password: string) => {
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+   if (!isPasswordValid) {
+     throw new Error("Invalid credentials");
+  }
+
+  const token = jwt.sign(
+    { userId: user.id },
+    process.env.JWT_SECRET!,
+    {expiresIn: "1d"}
+  )
+
+  const {password: _, ...userWithoutPassword} = user
+
+  return { token, user: userWithoutPassword };
+};
